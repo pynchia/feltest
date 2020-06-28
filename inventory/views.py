e31f5fc609ddc6db4017a86ec9f6d1084e256a22
mbb <pynchia@gmail.com>
2020-06-28 19:35:14 +0200

    template for index

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from django.http import HttpResponse
+
+from django.views import generic
+from datetime import date
+
+from models import Batch, Event, Product
+
 
 # Create your views here.
-from django.http import HttpResponse
 
 
+class IndexView(generic.ListView):
+    template_name = 'inventory/index.html'
+    context_object_name = 'freshness'
+
+    def get_queryset(self):
+        """Return three sets of batches, by exp_date:
+        Fresh, Expirying today, Expired
+        """
+        today = date.today()
+        return {
+            "fresh": Batch.objects.filter(exp_date__gt=today).order_by('exp_date'),
+            "today": Batch.objects.filter(exp_date=today).order_by('pur_date'),
+            "expired": Batch.objects.filter(exp_date__lt=today).order_by('exp_date'),
+        }
